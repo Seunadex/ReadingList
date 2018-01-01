090d8774571c3041f5b8fcc8f7df25860c6e9cee
seunadex <adekunleseun001@gmail.com>
2018-01-01 14:49:08 +0100

    extract json helper method

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -4,6 +4,8 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
   setup do
     Book.create!(title: 'Pragramtic Programmer', rating: 3)
     Book.create!(title: 'Awesome Seun', rating: 5)
+    Book.create!(title: 'YDKJ', rating: 4)
+    Book.create!(title: 'Ajax on Rails', rating: 3)
   end
 
   test 'listing books' do
@@ -12,6 +14,14 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
     assert_equal 200, response.status
     assert_equal Mime[:json], response.content_type
 
-    assert_equal Book.count, JSON.parse(response.body).size
+    assert_equal Book.count, json(response.body).size
+  end
+
+  test 'lists top rated books' do
+    get '/books?rating=3'
+
+    assert_equal 200, response.status
+    assert_equal Mime[:json], response.content_type
+    assert_equal 2, json(response.body).size
   end
 end
